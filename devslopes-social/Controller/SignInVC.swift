501dd4129e3398e5f1b7455abf34e19b7ffe3a36
Ahmad Ragab <a.ragab2012@hotmail.com>
2017-10-24 13:32:11 +0200

    Basic sign in layout complete

---
+++ devslopes-social/Controller/SignInVC.swift
@@ -0,0 +1,17 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Ahmad Ragab on 10/21/17.
+//  Copyright © 2017 Ahmad Ragab. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
