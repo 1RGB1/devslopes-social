b9335a26679e9ffc2216373be6ef0b98e59b1ac4
Ahmad Ragab <a.ragab2012@hotmail.com>
2017-10-26 00:23:05 +0200

    Connecting to Facebook into Firebase

--- devslopes-social/Controller/SignInVC.swift
+++ devslopes-social/Controller/SignInVC.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        // Authenticate with Facebook
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("Ahmad: Unable to authenticate with Facebook - \(String(describing: error?.localizedDescription))")
+            } else if result?.isCancelled == true {
+                print("Ahmad: User cancelled Facebook authentication")
+            } else {
+                print("Ahmad: Successfully authenticated with Facebook")
+                
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        
+        // Authenticate with Firebase
+        Auth.auth().signIn(with: credential) { (user, error) in
+            
+            if error != nil {
+                print("Ahmad: Unable to authenticate with Firebase - \(String(describing: error?.localizedDescription))")
+            } else {
+                print("Ahmad: Successfully authenticated with Firebase")
+            }
+        }
     }
 }
 
